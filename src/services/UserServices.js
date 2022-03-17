95a31b49ce01c49f2e753e8f5e0ba7fbca70262a
karamanemre <{775407}+{karamanemre}@users.noreply.github.com>
2022-03-18 00:19:15 +0300

    (login) and (changeLanguage) function  added

--- src/services/UserServices.js
+++ src/services/UserServices.js
@@ -1,8 +1,19 @@
 import axios from "axios";
 
 export default class UserServices {
-
   add(user) {
     return axios.post(`${process.env.REACT_APP_USER_BASE_URL_PATH}/add`, user);
+  }
+
+  //ikinci süüslü paranteze body kısmı gelir
+  // 3.yere doğrulama gerektiren yani password ve username gelecek
+  login(credential) {
+    console.log(credential);
+    return axios.post(`/api/auth/login`, credential);
+  }
+  // { auth: credential },
+
+  changeLanguage(language) {
+    return (axios.defaults.headers["accept-language"] = language);
   }
 }
