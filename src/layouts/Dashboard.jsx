bf075d3ea3b4b43763a665288f712798a482d8e9
karamanemre <{775407}+{karamanemre}@users.noreply.github.com>
2022-03-19 19:32:51 +0300

    revize

--- src/layouts/Dashboard.jsx
+++ src/layouts/Dashboard.jsx
@@ -2,18 +2,19 @@ import React from "react";
 import { Route, Routes } from "react-router";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import LanguageBar from "../components/LanguageBar";
 import Login from "../pages/Login";
 import Signup from "../pages/Signup";
+import Home from "./Home";
 
 function Dashboard() {
   return (
     <div>
       <ToastContainer position="bottom-right" />
-      <LanguageBar/>
       <Routes>
-        <Route exact path="/" element={ <Login />}></Route>
+        <Route exact path="/" element={ <Home />}></Route>
+        <Route exact path="/login" element={ <Login />}></Route>
         <Route exact path="/signup" element={<Signup />}></Route>
+        <Route exact path="*" element={<Home/>}></Route>
       </Routes>
     </div>
   );
