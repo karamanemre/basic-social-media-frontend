c9f1333a8fb2f81dd53be563d01ba02102ae2014
karamanemre <{775407}+{karamanemre}@users.noreply.github.com>
2022-03-19 19:32:34 +0300

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./css/style.scss";
 import Dashboard from "./layouts/Dashboard";
+import Navbar from "./layouts/Navbar";
 
 function App() {
   return (
       <div className="App">
+        <Navbar/>
         <Dashboard/>
         
       </div>
--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import './configuration/i18n'
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById('root')
 );
 
