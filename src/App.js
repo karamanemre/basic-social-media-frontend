99f0efee917b50afbc1f6632e169387e70d305f6
karamanemre <{775407}+{karamanemre}@users.noreply.github.com>
2022-03-19 13:17:01 +0300

    revize

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
-import logo from "./logo.svg";
-import Signup from "./pages/Signup";
 import "./css/style.scss";
-import { ToastContainer } from "react-toastify";
 import Dashboard from "./layouts/Dashboard";
 
 function App() {
   return (
-   
       <div className="App">
-        
         <Dashboard/>
+        
       </div>
  
   );
