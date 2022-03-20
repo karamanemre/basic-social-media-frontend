c8ad27252c07e5184bd1ce8cc24d2820c94c39fe
karamanemre <{775407}+{karamanemre}@users.noreply.github.com>
2022-03-20 23:24:01 +0300

    login page conditional

--- src/layouts/Dashboard.jsx
+++ src/layouts/Dashboard.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { Route, Routes } from "react-router";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -7,14 +8,18 @@ import Signup from "../pages/Signup";
 import Home from "./Home";
 
 function Dashboard() {
+
+  const {isAuthentication} = useSelector(state =>  state.user)
+
   return (
     <div>
       <ToastContainer position="bottom-right" />
       <Routes>
-        <Route exact path="/" element={ <Home />}></Route>
-        <Route exact path="/login" element={ <Login />}></Route>
-        <Route exact path="/signup" element={<Signup />}></Route>
-        <Route exact path="*" element={<Home/>}></Route>
+        <Route exact path="/" element={ <Home />}/>
+        {isAuthentication===false && <Route exact path="/login" element={ <Login />}/>}
+        {/* <Route exact path="/login" element={ <Login />}/> */}
+        <Route exact path="/signup" element={<Signup />}/>
+        <Route exact path="*" element={<Home/>}/>
       </Routes>
     </div>
   );
