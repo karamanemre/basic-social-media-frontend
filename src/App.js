9cc53b7d1d4a234ae20b8680f539dff5a7c8c7a7
karamanemre <{775407}+{karamanemre}@users.noreply.github.com>
2022-03-23 01:33:56 +0300

    Conditioned by path

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import { useLocation } from "react-router";
 import "./css/style.scss";
 import Dashboard from "./layouts/Dashboard";
+import Logo from "./layouts/Logo";
 import Navbar from "./layouts/Navbar";
 
 function App() {
+
+  const { pathname } = useLocation();
+console.log(pathname);
   return (
       <div className="App">
-        <Navbar/>
+
+       {pathname!=="/login" && pathname!=="/signup" && <Navbar/>} 
+       {(pathname==="/login" || pathname==="/signup") && <div style={{position:"absolute"}}>
+         <div style={{padding:"2rem"}}>
+           <Logo/>
+         </div>
+         </div>} 
         <Dashboard/>
         
       </div>
