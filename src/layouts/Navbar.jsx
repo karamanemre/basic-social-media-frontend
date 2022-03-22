d0c1fb2dd5bbfd2cc90b59b714bb690113c1e0a8
karamanemre <{775407}+{karamanemre}@users.noreply.github.com>
2022-03-23 01:35:29 +0300

    logo component

--- src/layouts/Navbar.jsx
+++ src/layouts/Navbar.jsx
@@ -1,30 +1,29 @@
 import React, { useState } from "react";
 import { withTranslation } from "react-i18next";
 import { useDispatch, useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link,useLocation } from "react-router-dom";
 import LanguageBar from "../layouts/LanguageBar";
 import SignIn from "./SignIn";
 import SignOut from "./SignOut";
 import { useTranslation } from "react-i18next";
+import Logo from "./Logo";
 
 function Navbar() {
+
+  const { pathname } = useLocation();
   const { t } = useTranslation();
   const { item, status, isAuthentication } = useSelector((state) => state.user);
+  
   return (
     <nav className="navbar navbar-dark">
       <div className="container">
         <div className="logo-container">
-          <Link to={"/"}>
-            <img
-              src="https://weglot.com/wp-content/themes/weglotv2/dist/images/Logo.svg"
-              alt="logo"
-            />
-          </Link>
+          <Logo/>
           <LanguageBar />
         </div>
 
         <div className="content ml-auto">
-          {isAuthentication ? <SignIn user={item} /> : <SignOut />}
+          {isAuthentication===true ? <SignIn user={item} /> : <SignOut />}
         </div>
       </div>
     </nav>
