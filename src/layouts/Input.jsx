27c43fe5e817eb1fcc6cba1db5dc0453c2a2bf62
karamanemre <{775407}+{karamanemre}@users.noreply.github.com>
2022-03-25 20:08:06 +0300

    style color

--- src/layouts/Input.jsx
+++ src/layouts/Input.jsx
@@ -4,7 +4,7 @@ import { withTranslation } from "react-i18next";
 
 function Input(props) {
   const { t } = useTranslation();
-  const { label, error, name, handleChange, value, id, inputType } = props;
+  const { label, error, name, handleChange, value, id, inputType,errorColor} = props;
   let className = error ? "form-control is-invalid" : "form-control";
   return (
     <div className="form-group mb-3">
@@ -17,7 +17,7 @@ function Input(props) {
         value={value}
         type={inputType}
       />
-      <small className="form-text error invalid-feedback">
+      <small className="form-text error invalid-feedback"  style={{color:errorColor}}>
         {error && error}
       </small>
     </div>
